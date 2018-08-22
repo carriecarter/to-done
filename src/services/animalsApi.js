d9f92110e0e9f537d7dfbefe069789f5a3e22597
carriecarter <carrie@carrie-carter.com>
2018-08-22 09:59:13 -0700

    get written in api

---
+++ src/services/animalsApi.js
@@ -0,0 +1,21 @@
+import { put, post, get, del } from './request';
+
+const URL = 'https://cc-to-done.firebaseio.com/';
+const ANIMALS_URL = `${URL}/animals`;
+
+const getAnimalUrl = key => `${ANIMALS_URL}/${key}.json`;
+
+export const getAnimals = () => {
+  return get (`${ANIMALS_URL}.json`)
+    .then(response => {
+      return response 
+        ?Object.keys(response).map(key => {
+          const each = response[key];
+          each.key = key;
+          return each;
+        })
+        : [];
+    });
+};
+
+export const addAnimal = (animal) =>
